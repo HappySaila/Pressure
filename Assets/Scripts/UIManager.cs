a0517a880fe9fc649320b4b745f42838919e7c84
HappySaila <granthaldanewilson836@gmail.com>
2017-07-28 23:43:38 +0200

    Main menu sweep feeature complete.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
-
+	public Animator mainMenu;
+	public Animator statsMenu;
+	public Animator settingsMenu;
 
 	// Use this for initialization
 	void Start () {
-		
+		mainMenu.SetTrigger ("SwipeIn");
 	}
 	
 	// Update is called once per frame
@@ -21,14 +23,26 @@ public class UIManager : MonoBehaviour {
 	}
 
 	public void btnStatsClicked(){
+		statsMenu.SetTrigger ("SwipeIn");
+		mainMenu.SetTrigger ("SwipeOut");
+	}
 
+	public void btnStatsBackClicked(){
+		statsMenu.SetTrigger ("SwipeOut");
+		mainMenu.SetTrigger ("SwipeIn");
 	}
 
 	public void btnSettingsClicked(){
+		settingsMenu.SetTrigger ("SwipeIn");
+		mainMenu.SetTrigger ("SwipeOut");
+	}
 
+	public void btnSettingsBackClicked(){
+		settingsMenu.SetTrigger ("SwipeOut");
+		mainMenu.SetTrigger ("SwipeIn");
 	}
 
 	public void btnQuitClicked(){
-
+		statsMenu.SetTrigger ("SwipeIn");
 	}
 }
