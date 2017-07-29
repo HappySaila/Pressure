ce706244f2953ee35770f5aaddffc8b3be973d17
mrrjam015 <mrrjam015@myuct.ac.za>
2017-07-29 10:36:27 +0200

    added light Particles

--- Assets/scripts/PresurePlateScript.cs
+++ Assets/scripts/PresurePlateScript.cs
@@ -6,43 +6,47 @@ public class PresurePlateScript : MonoBehaviour {
 
 	// Use this for initialization
 	public int plateNumberType;
+	public int numberOfChangePoints;//numberofblobs it can effect
 	private SpriteRenderer sr;
 	void Start () {
 		sr = GetComponent <SpriteRenderer>();
 
 
-		if(plateNumberType==null){
-			plateNumberType=Random.Range(0,5);
+		if(plateNumberType==null){//incase of null
+			plateNumberType=Random.Range(0,2);
 		}
 		switch (plateNumberType) {
+		case 0:
+			sr.color = Color.white;
+			break;
 		case 1:
-			sr.color = Color.red;
+			sr.color = Color.green;
 			break;
 
 		case 2:
 			sr.color = Color.blue;
 			break;
 
-		case 3:
-			sr.color = Color.green;
-			break;
-
-		case 4:
-			sr.color = Color.black;
-			break;
-
-		case 5:
-			sr.color = Color.magenta;
-			break;
-
 		}
-			
-
 
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+	void OnTriggerEnter2D(Collider2D blobTouched){
+
+
+			
+		FollowerMainScript blobTouchedScript=blobTouched.gameObject.GetComponentInChildren<FollowerMainScript>();
+		blobTouchedScript.setColourAndType (plateNumberType);
+			
+
+	}
+
+
 }
